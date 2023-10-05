65f891a0c8acf381275eff99c466633a53d79afa
janddrras <janddrras@gmail.com>
2023-10-05 13:08:11 +0300

    auth complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import Navbar from "@/components/landingPage/Navbar"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -13,7 +14,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="light">
-      <body className={cn("min-h-screen font-sans antialiased grainy", inter.className)}>{children}</body>
+      <body className={cn("min-h-screen font-sans antialiased grainy", inter.className)}>
+        <Navbar />
+        <main>{children}</main>
+      </body>
     </html>
   )
 }
