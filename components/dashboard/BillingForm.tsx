274f3d9691960f1a951863fda5fd47dbd50d2f2d
janddrras <janddrras@gmail.com>
2023-10-16 13:10:30 +0300

    stripe webhook

--- components/dashboard/BillingForm.tsx
+++ components/dashboard/BillingForm.tsx
@@ -1,9 +1,68 @@
+"use client"
+
+import { getUserSubscriptionPlan } from "@/lib/stripe"
+import MaxWidthWrapper from "../MaxWidthWrapper"
+import { format } from "date-fns"
+import { Loader2 } from "lucide-react"
+import { Button } from "../ui/button"
+import { Card, CardHeader, CardTitle, CardDescription, CardFooter } from "../ui/card"
+import { trpc } from "@/app/_trpc/client"
+import { useToast } from "../ui/use-toast"
+
 interface BillingFormProps {
-  subscriptionPlan: any
+  subscriptionPlan: Awaited<ReturnType<typeof getUserSubscriptionPlan>>
 }
 
 const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
-  return <div>BillingForm</div>
+  const { toast } = useToast()
+
+  const { mutate: createStripeSession, isLoading } = trpc.createStripeSession.useMutation({
+    onSuccess: ({ url }) => {
+      if (url) window.location.href = url
+      if (!url) {
+        toast({
+          title: "There was a problem...",
+          description: "Please try again in a moment",
+          variant: "destructive"
+        })
+      }
+    }
+  })
+
+  return (
+    <MaxWidthWrapper className="max-w-5xl">
+      <form
+        className="mt-12"
+        onSubmit={(e) => {
+          e.preventDefault()
+          createStripeSession()
+        }}
+      >
+        <Card>
+          <CardHeader>
+            <CardTitle>Subscription Plan</CardTitle>
+            <CardDescription>
+              You are currently on the <strong>{subscriptionPlan.name}</strong> plan.
+            </CardDescription>
+          </CardHeader>
+
+          <CardFooter className="flex flex-col items-start space-y-2 md:flex-row md:justify-between md:space-x-0">
+            <Button type="submit">
+              {isLoading ? <Loader2 className="mr-4 h-4 w-4 animate-spin" /> : null}
+              {subscriptionPlan.isSubscribed ? "Manage Subscription" : "Upgrade to PRO"}
+            </Button>
+
+            {subscriptionPlan.isSubscribed ? (
+              <p className="rounded-full text-xs font-medium">
+                {subscriptionPlan.isCanceled ? "Your plan will be canceled on " : "Your plan renews on"}
+                {format(subscriptionPlan.stripeCurrentPeriodEnd!, "dd.MM.yyyy")}.
+              </p>
+            ) : null}
+          </CardFooter>
+        </Card>
+      </form>
+    </MaxWidthWrapper>
+  )
 }
 
 export default BillingForm
